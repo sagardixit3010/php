d5dff5cad972c932d2f4f1bf9b0c438688cf508a
sagardixit3010 <sagar.dixit.3010@gmail.com>
2021-04-07 12:11:05 +0530

    Second commit

--- part5.php
+++ part5.php
@@ -5,10 +5,31 @@
     <title></title>
   </head>
   <body>
-<!--Arrays-->
+<!--Arrays
+An array stores multiple values in one single variable
+-->
+
     <?php
 
-    $friends=array("Sagar","Sanju","");
+    $friends=array("Sagar","Sanju","Sachin","Jim");
+    $friends[1]="Dwight";//arrays can be manipulated
+    echo $friends[1];
+
+    $friends[4]="Angel";
+    echo $friends[4];//Add at 4th index
+
+    //Count the number of elements in array
+
+    echo count($friends);
+
+
+
      ?>
+
+  <!--Checkboxes-->
+  
+
+
+
   </body>
 </html>
